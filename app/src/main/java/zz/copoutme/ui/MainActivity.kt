9b4e98696a2b3f998e75ebff7e90bf531a310766
zahar <zahar.zolotarev@gmail.com>
2020-10-16 19:43:55 +0300

    Иницаилизация проекта

---
+++ app/src/main/java/zz/copoutme/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package zz.copoutme.ui
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(TextView(this).apply {
+            text = "Hello World"
+        })
+    }
+}
